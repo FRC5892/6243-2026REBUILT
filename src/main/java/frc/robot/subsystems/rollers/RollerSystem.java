a057845a7738dc31804a175fb062072ee8fb55ff
Ruthie <s2169121@online.houstonisd.org>
2026-01-28 14:32:48 -0600

    Implement RollerSystem with periodic updates and alerts

--- src/main/java/frc/robot/subsystems/rollers/RollerSystem.java
+++ src/main/java/frc/robot/subsystems/rollers/RollerSystem.java
@@ -1,1 +1,74 @@
+package frc.robot.subsystems.rollers;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.Alert;
+import lombok.Setter;
+
+import frc.robot.Robot;
+import frc.robot.subsystems.rollers.RollerSystemIO.RollerSystemIOOutputs;
+import frc.robot.util.FullSubsystem;
+import frc.robot.util.LoggedTracer;
+
+import org.littletonrobotics.junction.Logger;
+
+public class RollerSystem extends FullSubsystem {
+  private final String name;
+  private final String inputsName;
+  private final RollerSystemIO io;
+  protected final RollerSystemIOInputsAutoLogged inputs =
+      new RollerSystemIOInputsAutoLogged();
+  private final RollerSystemIOOutputs outputs = new RollerSystemIOOutputs();
+
+  private final Debouncer motorConnectedDebouncer =
+      new Debouncer(0.5, Debouncer.DebounceType.kFalling);
+  private final Alert disconnected;
+
+  @Setter private double volts = 0.0;
+  @Setter private boolean brakeModeEnabled = true;
+
+  public RollerSystem(String name, String inputsName, RollerSystemIO io) {
+    this.name = name;
+    this.inputsName = inputsName;
+    this.io = io;
+
+    disconnected = new Alert(name + " motor disconnected!", Alert.AlertType.kWarning);
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs(inputsName, inputs);
+
+    disconnected.set(
+        Robot.showHardwareAlerts()
+            && !motorConnectedDebouncer.calculate(inputs.connected));
+
+    // Run roller
+    outputs.appliedVoltage = volts;
+
+    // Update brake mode
+    outputs.brakeModeEnabled = brakeModeEnabled;
+
+    // Record cycle time
+    LoggedTracer.record(name);
+
+    Logger.recordOutput(inputsName + "/BrakeModeEnabled", brakeModeEnabled);
+  }
+
+  @Override
+  public void periodicAfterScheduler() {
+    io.applyOutputs(outputs);
+  }
+
+  public double getTorqueCurrent() {
+    return inputs.torqueCurrentAmps;
+  }
+
+  public double getVelocity() {
+    return inputs.velocityRadsPerSec;
+  }
+
+  public void stop() {
+    volts = 0.0;
+  }
+}
