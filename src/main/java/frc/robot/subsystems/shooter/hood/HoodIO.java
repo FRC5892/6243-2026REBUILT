68325534b98ac62b43c36ecc4a066be7fb1029ff
Ruthie <s2169121@online.houstonisd.org>
2026-02-01 20:26:21 +0000

    just as many errors as the answer to life, the universe, and everthing

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodIO.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodIO.java
@@ -1,19 +1,22 @@
 package frc.robot.subsystems.shooter.hood;
 
 public interface HoodIO {
+
   class HoodIOInputs {
     public double positionRad = 0.0;
     public double velocityRadPerSec = 0.0;
   }
 
   class HoodIOOutputs {
-    public double positionRad = 0.0;
-    public Mode mode = Mode.POSITION;
+    public double positionRad = 0.0;          // radians for POSITION mode
+    public double voltagePercent = 0.0;       // [-1..1] for VOLTAGE mode
+    public double dutyCycleFeedforward = 0.0; // optional feedforward for POSITION mode
+    public Mode mode = Mode.POSITION;         // default mode
   }
 
   enum Mode {
     POSITION, // closed-loop position control
-    VOLTAGE // simple open-loop voltage
+    VOLTAGE   // simple open-loop voltage
   }
 
   void updateInputs(HoodIOInputs inputs);
