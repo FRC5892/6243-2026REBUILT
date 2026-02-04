167da9c5078256d07d8ba3a0107d922d095d6fae
Ruthie <s2169121@online.houstonisd.org>
2026-02-04 15:47:24 +0000

    progress i think

--- src/main/java/frc/robot/subsystems/rollers/RollerSystemIOReal.java
+++ src/main/java/frc/robot/subsystems/rollers/RollerSystemIOReal.java
@@ -1,18 +1,55 @@
-// Copyright (c) 2025-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
 package frc.robot.subsystems.rollers;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
+import edu.wpi.first.math.util.Units;
+
 public class RollerSystemIOReal implements RollerSystemIO {
-  public RollerSystemIOReal() {}
+
+  private final SparkMax motor;
+  private final RelativeEncoder encoder;
+
+  public RollerSystemIOReal(int motorId) {
+    motor = new SparkMax(motorId, MotorType.kBrushless);
+    encoder = motor.getEncoder();
+
+    // Initial motor config
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.smartCurrentLimit(40);
+    config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+
+    // Apply initial config (single-argument)
+    motor.configure(config);
+  }
 
   @Override
-  public void updateInputs(RollerSystemIOInputs inputs) {}
+  public void updateInputs(RollerSystemIOInputs inputs) {
+    inputs.connected = motor.getLastError() == null;
+    inputs.positionRads = Units.rotationsToRadians(encoder.getPosition());
+    inputs.velocityRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity());
+    inputs.appliedVoltage = motor.getAppliedOutput() * motor.getBusVoltage();
+    inputs.supplyCurrentAmps = motor.getOutputCurrent();
+    inputs.torqueCurrentAmps = motor.getOutputCurrent();
+    inputs.tempCelsius = motor.getMotorTemperature();
+  }
 
   @Override
-  public void applyOutputs(RollerSystemIOOutputs outputs) {}
+  public void applyOutputs(RollerSystemIOOutputs outputs) {
+    motor.setVoltage(outputs.appliedVoltage);
+
+    // Runtime brake mode toggle (2026 simplified API)
+    SparkMaxConfig runtimeConfig = new SparkMaxConfig();
+    runtimeConfig.idleMode(
+        outputs.brakeModeEnabled
+            ? SparkBaseConfig.IdleMode.kBrake
+            : SparkBaseConfig.IdleMode.kCoast
+    );
+
+    // Apply immediately, no deprecated enums
+    motor.configure(runtimeConfig);
+  }
 }
