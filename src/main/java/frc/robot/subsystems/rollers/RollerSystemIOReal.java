47dfac6fc1532c49da5d5f711c942c83905f9b28
Ruthie <s2169121@online.houstonisd.org>
2026-02-04 16:12:26 +0000

    the best its gotten so far...

--- src/main/java/frc/robot/subsystems/rollers/RollerSystemIOReal.java
+++ src/main/java/frc/robot/subsystems/rollers/RollerSystemIOReal.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems.rollers;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig;
 
 import edu.wpi.first.math.util.Units;
 
@@ -16,21 +14,14 @@ public class RollerSystemIOReal implements RollerSystemIO {
   public RollerSystemIOReal(int motorId) {
     motor = new SparkMax(motorId, MotorType.kBrushless);
     encoder = motor.getEncoder();
-
-    // Initial motor configuration
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.smartCurrentLimit(40);
-    config.idleMode(SparkBaseConfig.IdleMode.kBrake);
-
-    // Apply config (2026 API)
-    motor.configure(config);
   }
 
   @Override
   public void updateInputs(RollerSystemIOInputs inputs) {
     inputs.connected = motor.getLastError() == null;
     inputs.positionRads = Units.rotationsToRadians(encoder.getPosition());
-    inputs.velocityRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity());
+    inputs.velocityRadsPerSec =
+        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity());
     inputs.appliedVoltage = motor.getAppliedOutput() * motor.getBusVoltage();
     inputs.supplyCurrentAmps = motor.getOutputCurrent();
     inputs.torqueCurrentAmps = motor.getOutputCurrent();
@@ -41,14 +32,9 @@ public class RollerSystemIOReal implements RollerSystemIO {
   public void applyOutputs(RollerSystemIOOutputs outputs) {
     motor.setVoltage(outputs.appliedVoltage);
 
-    // Toggle brake/coast at runtime
-    SparkMaxConfig runtimeConfig = new SparkMaxConfig();
-    runtimeConfig.idleMode(
-        outputs.brakeModeEnabled ? SparkBaseConfig.IdleMode.kBrake
-                                 : SparkBaseConfig.IdleMode.kCoast
-    );
-
-    // 2026 API: single-argument configure
-    motor.configure(runtimeConfig);
+    // toggle brake/coast dynamically
+    motor.setIdleMode(outputs.brakeModeEnabled
+        ? com.revrobotics.spark.SparkLowLevel.IdleMode.kBrake
+        : com.revrobotics.spark.SparkLowLevel.IdleMode.kCoast);
   }
 }
