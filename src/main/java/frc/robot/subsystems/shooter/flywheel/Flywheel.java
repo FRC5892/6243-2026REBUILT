a3cb383d0cae017f84812b537cce33b092a46e83
Ruthie <s2169121@online.houstonisd.org>
2026-02-06 14:16:53 +0000

    stolen code is best code

---
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/Flywheel.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems.shooter.flywheel;
+
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import frc.robot.subsystems.shooter.ShotCalculator;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LoggedTalon.LoggedTalonFX;
+import frc.robot.util.LoggedTunableMeasure;
+import lombok.Getter;
+import org.littletonrobotics.junction.AutoLogOutput;
+
+public class Flywheel extends SubsystemBase {
+
+  private final LoggedTalonFX motor;
+
+  private final MotionMagicVelocityTorqueCurrentFOC mmControl =
+      new MotionMagicVelocityTorqueCurrentFOC(0);
+  @Getter @AutoLogOutput private boolean atSetpoint = false;
+  private final LoggedTunableMeasure<MutAngularVelocity> tolerance =
+      new LoggedTunableMeasure<>("Flywheel/Tolerance", RPM.mutable(5));
+
+  public Flywheel(LoggedTalonFX motor) {
+    this.motor = motor;
+    setDefaultCommand(aimCommand());
+  }
+
+  public void setSetpoint(AngularVelocity velocity) {
+    motor.setControl(mmControl.withVelocity(velocity));
+  }
+
+  public Command aimCommand() {
+    return run(
+        () -> {
+          setSetpoint(
+              RotationsPerSecond.of(
+                  ShotCalculator.getInstance().calculateShot().flywheelSpeedRotPerSec()));
+        });
+  }
+
+  @Override
+  public void periodic() {
+    motor.periodic();
+    atSetpoint = motor.atSetpoint(mmControl.getVelocityMeasure(), tolerance.get());
+    ShotCalculator.getInstance().clearCache();
+  }
+}
