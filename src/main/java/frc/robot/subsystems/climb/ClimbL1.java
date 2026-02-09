9cdc4bad49e9859af9556d0cd37626ee6a43c565
Ruthie-FRC <s2169121@online.houstonisd.org>
2026-02-09 16:23:57 -0600

    im really annoyed rn so my code doenst get to be happy either

--- src/main/java/frc/robot/subsystems/climb/ClimbL1.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbL1.java
@@ -1,3 +1,38 @@
 package frc.robot.subsystems.climb;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbL1 {}
+public class ClimbL1 extends SubsystemBase {
+    //TODO: switch to krackens
+    //Climber Variables
+    /* private final SparkMax climberLeft = new SparkMax(SubsystemConstants.kLeftClimber, MotorType.kBrushless);
+    private final SparkMax climberRight = new SparkMax(SubsystemConstants.kRightClimber, MotorType.kBrushless);
+
+    //Left Climber goes Up
+    public void climberLeftUp(){
+        climberLeft.set(-.75);
+    }
+    //Left Climber goes Down
+    public void climberLeftDown(){
+        climberLeft.set(.75);
+    }
+
+    //Right Climber goes Up
+    public void climberRightUp(){
+        climberRight.set(.75);
+    }
+
+    //Right Climber goes DOwn
+    public void climberRightDown(){
+        climberRight.set(-.75);
+    }
+
+    //Right Climber no go
+    public void stopRightMotor(){
+        climberRight.set(0);
+    }
+
+    //Left climber no go
+    public void stopLeftMotor(){
+        climberLeft.set(0);
+    } */
+} 
