71d984ebb8158a5dd9b52e285e4706553ac7dd2f
Ruthie-FRC <s2169121@online.houstonisd.org>
2026-02-09 17:06:02 -0600

    there i made a l1 happy????

--- src/main/java/frc/robot/subsystems/climb/ClimbL1.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbL1.java
@@ -1,38 +1,86 @@
 package frc.robot.subsystems.climb;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SlotConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LoggedTalon.TalonFX.LoggedTalonFX;
 
 public class ClimbL1 extends SubsystemBase {
-    //TODO: switch to krackens
-    //Climber Variables
-    /* private final SparkMax climberLeft = new SparkMax(SubsystemConstants.kLeftClimber, MotorType.kBrushless);
-    private final SparkMax climberRight = new SparkMax(SubsystemConstants.kRightClimber, MotorType.kBrushless);
 
-    //Left Climber goes Up
-    public void climberLeftUp(){
-        climberLeft.set(-.75);
-    }
-    //Left Climber goes Down
-    public void climberLeftDown(){
-        climberLeft.set(.75);
-    }
+  private final LoggedTalonFX climberLeft;
+  private final LoggedTalonFX climberRight;
 
-    //Right Climber goes Up
-    public void climberRightUp(){
-        climberRight.set(.75);
-    }
+  private final DutyCycleOut leftRequest = new DutyCycleOut(0);
+  private final DutyCycleOut rightRequest = new DutyCycleOut(0);
 
-    //Right Climber goes DOwn
-    public void climberRightDown(){
-        climberRight.set(-.75);
-    }
+  public ClimbL1(LoggedTalonFX climberLeft, LoggedTalonFX climberRight) {
+    this.climberLeft = climberLeft;
+    this.climberRight = climberRight;
 
-    //Right Climber no go
-    public void stopRightMotor(){
-        climberRight.set(0);
-    }
+    var config =
+        new TalonFXConfiguration()
+            .withSlot0(new Slot0Configs().withKP(0).withKI(0).withKD(0))
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withNeutralMode(NeutralModeValue.Brake)
+                    .withInverted(InvertedValue.Clockwise_Positive))
+            .withCurrentLimits(new CurrentLimitsConfigs().withStatorCurrentLimit(60));
 
-    //Left climber no go
-    public void stopLeftMotor(){
-        climberLeft.set(0);
-    } */
-} 
+    this.climberLeft.withConfig(config).withMMPIDTuning(SlotConfigs.from(config.Slot0), null);
+    this.climberRight.withConfig(config).withMMPIDTuning(SlotConfigs.from(config.Slot0), null);
+  }
+
+  public void climberLeftUp() {
+    climberLeft.setControl(leftRequest.withOutput(-0.75));
+  }
+
+  public void climberLeftDown() {
+    climberLeft.setControl(leftRequest.withOutput(0.75));
+  }
+
+  public void climberRightUp() {
+    climberRight.setControl(rightRequest.withOutput(0.75));
+  }
+
+  public void climberRightDown() {
+    climberRight.setControl(rightRequest.withOutput(-0.75));
+  }
+
+  public void stopLeftMotor() {
+    climberLeft.setControl(leftRequest.withOutput(0));
+  }
+
+  public void stopRightMotor() {
+    climberRight.setControl(rightRequest.withOutput(0));
+  }
+
+  public Command leftUpCommand() {
+    return startEnd(this::climberLeftUp, this::stopLeftMotor);
+  }
+
+  public Command leftDownCommand() {
+    return startEnd(this::climberLeftDown, this::stopLeftMotor);
+  }
+
+  public Command rightUpCommand() {
+    return startEnd(this::climberRightUp, this::stopRightMotor);
+  }
+
+  public Command rightDownCommand() {
+    return startEnd(this::climberRightDown, this::stopRightMotor);
+  }
+
+  public Command stopAllCommand() {
+    return runOnce(() -> {
+      stopLeftMotor();
+      stopRightMotor();
+    });
+  }
+}
