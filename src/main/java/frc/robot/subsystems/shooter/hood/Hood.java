75aa2d8e6851fcbc7c1341528252a2bae0ac4341
Ruthie <s2169121@online.houstonisd.org>
2026-02-18 19:06:57 +0000

    bye

--- src/main/java/frc/robot/subsystems/shooter/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/Hood.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -76,7 +75,6 @@ public class Hood extends SubsystemBase {
   @AutoLogOutput private Rectangle2d[] trenchAreas = new Rectangle2d[4];
 
   /* Control  Requests*/
-  private final NeutralOut neutralControl = new NeutralOut();
   private final MotionMagicVoltage mmControl = new MotionMagicVoltage(targetPosition);
 
   public Hood(LoggedTalonFX motor, LoggedDIO reverseLimit, LoggedDIO forwardLimit) {
