637e1749e038ce628516ee7dd8cc6a3c4d50b8ac
Ruthie-FRC <s2169121@online.houstonisd.org>
2026-02-21 10:12:41 -0600

    yay?

--- src/main/java/frc/robot/commands/shooter/ShootCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShootCommand.java
@@ -7,14 +7,14 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 public class ShootCommand {
 
-    /** 
-     * Creates a functional command to handle shooting. 
+    /**
+     * Creates a functional command to handle shooting.
+     *
      * @param shooter the Shooter subsystem
      * @param indexer the Indexer subsystem
-     * @param voltage the fixed voltage to run the indexer at
      * @return a Command ready to be scheduled
      */
-    public static Command shoot(Shooter shooter, Indexer indexer, double voltage) {
+    public static Command shoot(Shooter shooter, Indexer indexer) {
         return shooter.getFlywheel().run(
             () -> {
                 // Calculate shot
@@ -22,18 +22,18 @@ public class ShootCommand {
 
                 // Apply flywheel and hood setpoints
                 shooter.getFlywheel().setSetpoints(
-                    RotationsPerSecond.of(shot.flywheelSpeedRotPerSec()),
-                    RotationsPerSecond.of(shot.flywheelSpeedRotPerSec())
-);
+                        RotationsPerSecond.of(shot.flywheelSpeedRotPerSec()),
+                        RotationsPerSecond.of(shot.flywheelSpeedRotPerSec())
+                );
                 shooter.getHood().requestAngle(shot.hoodAngle());
 
                 // Only run indexer if flywheel and hood are ready, and robot is facing correct rotation
-                boolean ready = shooter.isReadyToShoot();
-
-                if (ready) {
-                    indexer.setVoltage(voltage);
+                if (shooter.isReadyToShoot()) {
+                    // Run feeder + indexer rollers forward
+                    indexer.runForShooting().schedule();
                 } else {
-                    indexer.setVoltage(0);
+                    // Stop all indexer rollers if not ready
+                    indexer.stopAll().schedule();
                 }
             }
         );
