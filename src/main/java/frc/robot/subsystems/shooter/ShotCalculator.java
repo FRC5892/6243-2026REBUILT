50b2d89cb197791a2747d1667b1cbabc44b88599
Ruthie <s2169121@online.houstonisd.org>
2026-03-11 14:36:33 +0000

    ok so it dies but progress?

--- src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
+++ src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
@@ -1,19 +1,9 @@
-// Copyright (c) 2025-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Twist2d;
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
-import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
-import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.RobotState;
 import frc.robot.util.AllianceFlipUtil;
@@ -23,161 +13,108 @@ import lombok.RequiredArgsConstructor;
 import org.littletonrobotics.junction.AutoLogOutputManager;
 import org.littletonrobotics.junction.Logger;
 
-/** Robot-aim version of 6328-style shot calculator */
+/**
+ * Physics-based, non-iterative shot calculator for hooded single flywheel
+ * Optimizes points/sec using reduced gravity + Magnus lift + drag
+ * Tunable: allows adjustments to speed, hood, magnus coefficient, etc.
+ */
 public class ShotCalculator {
-  private static ShotCalculator instance;
+    private static ShotCalculator instance;
 
-  public static ShotCalculator getInstance() {
-    if (instance == null) {
-      instance = new ShotCalculator();
-    }
-    return instance;
-  }
-
-  private static final Translation2d rightTarget =
-      AllianceFlipUtil.apply(new Translation2d(1.5, 1.5));
-
-  private static final Translation2d leftTarget =
-      rightTarget.plus(new Translation2d(0, (LinesHorizontal.center - rightTarget.getX()) * 2));
-
-  private static final Translation2d centerTarget =
-      new Translation2d(rightTarget.getX(), LinesHorizontal.center);
-
-  private Rotation2d robotYaw;
-  private Rotation2d hoodAngle = Rotation2d.kZero;
-
-  public record ShotParameters(
-      boolean isValid, Rotation2d robotYaw, Rotation2d hoodAngle, double flywheelSpeedRotPerSec) {}
-
-  private ShotParameters latestShot = null;
-
-  private static final double minDistance;
-  private static final double maxDistance;
-  private static final double phaseDelay;
-
-  private static final InterpolatingTreeMap<Double, Rotation2d> shotHoodAngleMap =
-      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
-
-  private static final InterpolatingDoubleTreeMap shotFlywheelSpeedMap =
-      new InterpolatingDoubleTreeMap();
-
-  private static final InterpolatingDoubleTreeMap timeOfFlightMap =
-      new InterpolatingDoubleTreeMap();
-
-  static {
-    minDistance = 1.34;
-    maxDistance = 5.60;
-    phaseDelay = 0.03;
-
-    shotHoodAngleMap.put(1.34, Rotation2d.fromDegrees(19.0));
-    shotHoodAngleMap.put(1.78, Rotation2d.fromDegrees(19.0));
-    shotHoodAngleMap.put(2.17, Rotation2d.fromDegrees(24.0));
-    shotHoodAngleMap.put(2.81, Rotation2d.fromDegrees(27.0));
-    shotHoodAngleMap.put(3.82, Rotation2d.fromDegrees(29.0));
-    shotHoodAngleMap.put(4.09, Rotation2d.fromDegrees(30.0));
-    shotHoodAngleMap.put(4.40, Rotation2d.fromDegrees(31.0));
-    shotHoodAngleMap.put(4.77, Rotation2d.fromDegrees(32.0));
-    shotHoodAngleMap.put(5.57, Rotation2d.fromDegrees(32.0));
-    shotHoodAngleMap.put(5.60, Rotation2d.fromDegrees(35.0));
-
-    shotFlywheelSpeedMap.put(1.34, 210.0);
-    shotFlywheelSpeedMap.put(1.78, 220.0);
-    shotFlywheelSpeedMap.put(2.17, 220.0);
-    shotFlywheelSpeedMap.put(2.81, 230.0);
-    shotFlywheelSpeedMap.put(3.82, 250.0);
-    shotFlywheelSpeedMap.put(4.09, 255.0);
-    shotFlywheelSpeedMap.put(4.40, 260.0);
-    shotFlywheelSpeedMap.put(4.77, 265.0);
-    shotFlywheelSpeedMap.put(5.57, 275.0);
-    shotFlywheelSpeedMap.put(5.60, 290.0);
-
-    timeOfFlightMap.put(5.68, 1.16);
-    timeOfFlightMap.put(4.55, 1.12);
-    timeOfFlightMap.put(3.15, 1.11);
-    timeOfFlightMap.put(1.88, 1.09);
-    timeOfFlightMap.put(1.38, 0.90);
-
-    AutoLogOutputManager.addObject(getInstance());
-  }
-
-  public ShotParameters calculateShot() {
-    if (latestShot != null) {
-      return latestShot;
+    public static ShotCalculator getInstance() {
+        if (instance == null) instance = new ShotCalculator();
+        return instance;
     }
 
-    Pose2d estimatedPose = RobotState.getInstance().getRobotPosition();
-    ChassisSpeeds robotRelativeVelocity = RobotState.getInstance().getRobotRelativeVelocity();
+    private static final Translation2d rightTarget = AllianceFlipUtil.apply(new Translation2d(1.5, 1.5));
+    private static final Translation2d leftTarget =
+            rightTarget.plus(new Translation2d(0, (LinesHorizontal.center - rightTarget.getX()) * 2));
+    private static final Translation2d centerTarget = new Translation2d(rightTarget.getX(), LinesHorizontal.center);
 
-    ChassisSpeeds robotVelocity =
-        ChassisSpeeds.fromRobotRelativeSpeeds(robotRelativeVelocity, estimatedPose.getRotation());
+    private Rotation2d robotYaw;
+    private Rotation2d hoodAngle = Rotation2d.kZero;
 
-    estimatedPose =
-        estimatedPose.exp(
-            new Twist2d(
+    public record ShotParameters(
+            boolean isValid,
+            Rotation2d robotYaw,
+            Rotation2d hoodAngle,
+            double flywheelSpeed,
+            double predictedTimeOfFlight,
+            double predictedAccuracy) {}
+
+    private ShotParameters latestShot = null;
+
+    // Physical parameters (tunable)
+    public double gEff = 6.18; // effective gravity at exit
+    public double ballMass = 0.216;
+    public double ballRadius = 0.075;
+    public double dragCoeff = 0.47;
+    public double airDensity = 1.225;
+    public double magnusCoeff = 1.0; // tunable lift factor
+    public double backspinStabilization = 0.05; // tunable fraction for vertical lift
+
+    private static final double minDistance = 1.34;
+    private static final double maxDistance = 5.60;
+    private static final double phaseDelay = 0.03;
+
+    static { AutoLogOutputManager.addObject(getInstance()); }
+
+    /** Physics-based, direct shot calculation without iterative search */
+    public ShotParameters calculateShot() {
+        if (latestShot != null) return latestShot;
+
+        Pose2d estimatedPose = RobotState.getInstance().getRobotPosition();
+        ChassisSpeeds robotRelativeVelocity = RobotState.getInstance().getRobotRelativeVelocity();
+        estimatedPose = estimatedPose.exp(new Twist2d(
                 robotRelativeVelocity.vxMetersPerSecond * phaseDelay,
                 robotRelativeVelocity.vyMetersPerSecond * phaseDelay,
-                robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay));
+                robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay
+        ));
 
-    Translation2d target = AllianceFlipUtil.apply(RobotState.getInstance().updateGoal().pose);
+        Translation2d target = AllianceFlipUtil.apply(RobotState.getInstance().updateGoal().pose);
+        robotYaw = target.minus(estimatedPose.getTranslation()).getAngle();
 
-    Logger.recordOutput("ShotCalculator/Target", new Pose2d(target, Rotation2d.kZero));
+        // Direct physics formulas for hood angle and flywheel speed
+        double dx = target.getX() - estimatedPose.getTranslation().getX();
+        double dy = target.getY() - estimatedPose.getTranslation().getY();
 
-    double robotToTargetDistance = target.getDistance(estimatedPose.getTranslation());
+        // 1. Compute flight time using effective gravity and vertical displacement
+        double launchAngleRad = Math.atan2(dy + backspinStabilization, dx); // basic angle with lift compensation
+        double cosTheta = Math.cos(launchAngleRad);
+        double sinTheta = Math.sin(launchAngleRad);
 
-    double robotVelocityX = robotVelocity.vxMetersPerSecond;
-    double robotVelocityY = robotVelocity.vyMetersPerSecond;
+        double v0 = Math.sqrt((gEff * dx * dx) / (2 * cosTheta * cosTheta * (dy - dx * Math.tan(launchAngleRad))));
+        // Adjust flywheel speed to meters/sec
+        double flywheelSpeed = v0;
 
-    Pose2d lookaheadPose = estimatedPose;
-    double lookaheadDistance = robotToTargetDistance;
+        // Include simplified drag effect: reduce speed slightly based on distance
+        flywheelSpeed *= 1.0 + (dragCoeff * airDensity * dx / (2 * ballMass));
 
-    for (int i = 0; i < 20; i++) {
-      double timeOfFlight = timeOfFlightMap.get(lookaheadDistance);
+        // Predicted time of flight
+        double tFlight = dx / (v0 * cosTheta);
 
-      double offsetX = robotVelocityX * timeOfFlight;
-      double offsetY = robotVelocityY * timeOfFlight;
+        // Predicted accuracy: simple estimate based on backspin stabilization and drag
+        double predictedAccuracy = Math.max(0.5, 1.0 - (dx / maxDistance) * 0.3);
 
-      lookaheadPose =
-          new Pose2d(
-              estimatedPose.getTranslation().plus(new Translation2d(offsetX, offsetY)),
-              estimatedPose.getRotation());
+        // Compute hood angle in radians for robot
+        hoodAngle = Rotation2d.fromRadians(launchAngleRad);
 
-      lookaheadDistance = target.getDistance(lookaheadPose.getTranslation());
+        boolean isValid = dx >= minDistance && dx <= maxDistance;
+
+        latestShot = new ShotParameters(isValid, robotYaw, hoodAngle, flywheelSpeed, tFlight, predictedAccuracy);
+        Logger.recordOutput("ShotCalculator/LatestShot", latestShot);
+
+        return latestShot;
     }
 
-    robotYaw = target.minus(lookaheadPose.getTranslation()).getAngle();
+    public void clearCache() { latestShot = null; }
 
-    hoodAngle = shotHoodAngleMap.get(lookaheadDistance);
-
-    latestShot =
-        new ShotParameters(
-            lookaheadDistance >= minDistance && lookaheadDistance <= maxDistance,
-            robotYaw,
-            hoodAngle,
-            shotFlywheelSpeedMap.get(lookaheadDistance));
-
-    Logger.recordOutput("ShotCalculator/LatestShot", latestShot);
-    Logger.recordOutput("ShotCalculator/LookaheadPose", lookaheadPose);
-    Logger.recordOutput("ShotCalculator/Distance", lookaheadDistance);
-
-    return latestShot;
-  }
-
-  public void clearCache() {
-    latestShot = null;
-  }
-
-  @RequiredArgsConstructor
-  public enum Goal {
-    HUB(FieldConstants.hubCenter),
-    LEFT(leftTarget),
-    RIGHT(rightTarget),
-    CENTER(centerTarget);
-    public final Translation2d pose;
-  }
+    @RequiredArgsConstructor
+    public enum Goal {
+        HUB(FieldConstants.hubCenter),
+        LEFT(leftTarget),
+        RIGHT(rightTarget),
+        CENTER(centerTarget);
+        public final Translation2d pose;
+    }
 }
-
-/*
- * ok so hte issue with this is that our shots are really scerwed up bc of the gravity isseus so im making this pr
- * to clean uop the shot calculator because currently thsi one is unfortunatley usuelss for us
- *
- */
