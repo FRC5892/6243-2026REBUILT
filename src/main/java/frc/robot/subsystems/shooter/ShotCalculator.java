30d303769d95cd6dbe9a66dc0c6fb87bdd31c278
Ruthie <s2169121@online.houstonisd.org>
2026-03-11 13:39:01 +0000

    build code

--- src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
+++ src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
@@ -177,7 +177,7 @@ public class ShotCalculator {
 }
 
 /*
-* ok so hte issue with this is that our shots are really scerwed up bc of the gravity isseus so im making this pr 
-* to clean uop the shot calculator because currently thsi one is unfortunatley usuelss for us
-*
-*/
+ * ok so hte issue with this is that our shots are really scerwed up bc of the gravity isseus so im making this pr
+ * to clean uop the shot calculator because currently thsi one is unfortunatley usuelss for us
+ *
+ */
